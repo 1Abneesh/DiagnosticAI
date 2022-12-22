1709fad4b89a98f490b911ad9cc372898cb709af
1Abneesh <01abneeshkumar@gmail.com>
2022-12-22 18:15:32 +0530

    changes style.css position

--- app.py
+++ app.py
@@ -115,12 +115,12 @@ if (selected == "Contact"):
 
     st.markdown(contact_form,unsafe_allow_html=True)
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
-local_css("style\style.css")
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    
+    
+    local_css("style.css")
 
 
 
